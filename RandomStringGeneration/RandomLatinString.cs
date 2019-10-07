87865f65e338ca7805d0717ae335503435a5d87c
Никита Денисов <nikitadenisovvalerevich@gmail.com>
2019-10-07 11:57:59 +0500

    Moved this project to a different directory

---
+++ RandomStringGeneration/RandomLatinString.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomStringGeneration
+{
+    public class RandomLatinString : RandomString
+    {
+        public RandomLatinString() : base() { }
+
+        public override string Generate(int length)
+        {
+            if (length == 0)
+            {
+                return string.Empty;
+            }
+
+            if (length < 0)
+            {
+                Exception e = new FormatException("Длина строки не может быть меньше нуля");
+                throw (e);
+            }
+            char[] protoString = new char[length];
+            for (int i = 0; i < length; i++)
+                protoString[i] = alphabet[rNG.Next(0, alphabet.Length - 1)];
+            return new string(protoString);
+        }
+    }
+}
---
+++ RandomStringGeneration/RandomString.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomStringGeneration
+{
+    public abstract class RandomString
+    {
+        protected Random rNG;
+        protected string alphabet;
+        abstract public string Generate(int length);
+
+        public RandomString()
+        {
+            rNG = new Random();
+            this.alphabet = "abcdefghijklmopqrsuvwxyz";
+        }
+    }
+}
